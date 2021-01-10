00b5c1a685349b64cdfbbd8b059167167a35c77b
siahj1124 <s10205453@connect.np.edu.sg>
2021-01-10 22:12:47 +0800

    testing where my file gets commited to

---
+++ script.js
@@ -0,0 +1,12 @@
+$('#nav-covid-tab').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
+
+$('#nav-dengue-tab').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
+
+
+ 
